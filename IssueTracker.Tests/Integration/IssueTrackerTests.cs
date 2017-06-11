6285475b7b01419ee91bf987418495dcb5c7761c
MarcStan <marcstan@live.com>
2017-06-12 00:53:43 +0200

    added integration tests for actual issue tracker implementation

--- IssueTracker.Tests/Integration/IssueTrackerTests.cs
+++ IssueTracker.Tests/Integration/IssueTrackerTests.cs
@@ -5,4 +5,59 @@ using System.IO;
 
 namespace IssueTracker.Tests.Integration
 {
+    /// <summary>
+    /// Asserts that the output of issue tracker commands is as expected.
+    /// </summary>
+    [TestFixture]
+    public class IssueTrackerTests
+    {
+        [Test]
+        public void CreateNewIssueTrackerProjectInEmptyDirectory()
+        {
+            var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(path);
+
+            var it = new IssueTracker(path);
+            it.WorkingDirectoryIsIssueTracker.Should().BeFalse();
+
+            it.InitializeNewProject();
+            it.WorkingDirectoryIsIssueTracker.Should().BeTrue();
+
+            Directory.Delete(path, true);
+        }
+
+        [Test]
+        public void CreateNewIssueTrackerProjectInExistingIssueTracker()
+        {
+            var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(path);
+
+            var it = new IssueTracker(path);
+
+            it.InitializeNewProject();
+            it.WorkingDirectoryIsIssueTracker.Should().BeTrue();
+
+            // does nothing because it aborts
+            it.InitializeNewProject();
+            it.WorkingDirectoryIsIssueTracker.Should().BeTrue();
+
+            Directory.Delete(path, true);
+        }
+
+        [Test]
+        public void AddNewIssueInEmptyDirectory()
+        {
+            var path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(path);
+
+            var it = new IssueTracker(path);
+            it.WorkingDirectoryIsIssueTracker.Should().BeFalse();
+
+            new Action(() => it.AddIssue("foo", "bar", null)).ShouldThrow<NotSupportedException>();
+
+            it.WorkingDirectoryIsIssueTracker.Should().BeFalse();
+
+            Directory.Delete(path, true);
+        }
+    }
 }
