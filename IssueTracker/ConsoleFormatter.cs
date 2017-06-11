4b6385952ce8ac0a4b05bdd443ec11bdfe1918ab
MarcStan <marcstan@live.com>
2017-06-11 23:24:05 +0200

    added actual display function

---
+++ IssueTracker/ConsoleFormatter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IssueTracker
+{
+    public static class ConsoleFormatter
+    {
+        /// <summary>
+        /// Converts a List of string arrays to a string where each element in each line is correctly padded.
+        /// Make sure that each array contains the same amount of elements!
+        /// - Example without:
+        /// Title Name Street
+        /// Mr. Roman Sesamstreet
+        /// Mrs. Claudia Abbey Road
+        /// - Example with:
+        /// Title   Name      Street
+        /// Mr.     Roman     Sesamstreet
+        /// Mrs.    Claudia   Abbey Road
+        /// <param name="lines">List lines, where each line is an array of elements for that line.</param>
+        /// <param name="padding">Additional padding between each element (default = 1)</param>
+        /// </summary>
+        /// <remarks>
+        /// https://stackoverflow.com/a/27399595
+        /// </remarks>
+        public static string PadElementsInLines(List<string[]> lines, int padding = 1)
+        {
+            // Calculate maximum numbers for each element accross all lines
+            var numElements = lines[0].Length;
+            var maxValues = new int[numElements];
+            for (int i = 0; i < numElements; i++)
+            {
+                maxValues[i] = lines.Max(x => (x.Length > i + 1 && x[i] != null ? x[i].Length : 0)) + padding;
+            }
+            var sb = new StringBuilder();
+            // Build the output
+            bool isFirst = true;
+            foreach (var line in lines)
+            {
+                if (!isFirst)
+                {
+                    sb.AppendLine();
+                }
+                isFirst = false;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    var value = line[i];
+                    // Append the value with padding of the maximum length of any value for this element
+                    if (value != null)
+                    {
+                        sb.Append(value.PadRight(maxValues[i]));
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
--- IssueTracker/IssueTracker.cs
+++ IssueTracker/IssueTracker.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using IssueTracker.Extensions;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace IssueTracker
 {
@@ -11,6 +13,7 @@ namespace IssueTracker
     public class IssueTracker
     {
         private readonly string _workingDirectory;
+        private const string RootFile = ".issues";
 
         /// <summary>
         /// Creates a new instance that works in the provided directory.
@@ -32,14 +35,22 @@ namespace IssueTracker
         /// <summary>
         /// Gets whether the working directory is an issue tracker.
         /// </summary>
-        public virtual bool WorkingDirectoryIsIssueTracker => File.Exists(Path.Combine(_workingDirectory, ".issues"));
+        public virtual bool WorkingDirectoryIsIssueTracker => File.Exists(Path.Combine(_workingDirectory, RootFile));
 
         /// <summary>
         /// Creates a new project in the current directory.
         /// </summary>
         public virtual void InitializeNewProject()
         {
-            throw new System.NotImplementedException();
+            if (WorkingDirectoryIsIssueTracker)
+            {
+                Console.WriteLine("Already an issue tracking directory!");
+                return;
+            }
+            var user = CurrentUser;
+            File.WriteAllText(RootFile, $"owner={user}");
+
+            Console.WriteLine($"New issue project created for owner {user}!");
         }
 
         /// <summary>
@@ -48,7 +59,97 @@ namespace IssueTracker
         /// <param name="filters"></param>
         public virtual void ListIssues(List<FilterValue> filters)
         {
-            throw new NotImplementedException();
+            AssertIssueTracker();
+
+            var issues = Storage.LoadIssues();
+            bool stateWasFiltered = false;
+            foreach (var f in filters)
+            {
+                if (f.FilterType == Filter.IssueState)
+                    stateWasFiltered = true;
+
+                ApplyFilter(f, issues);
+            }
+            if (!issues.Any())
+            {
+                Console.WriteLine("Found no matching issues!");
+                return;
+            }
+            Console.WriteLine($"Found {issues.Count} matching issues:");
+            // if a statefilter exists, only the specific state will be displayed -> no point in printing it again in each line
+            // the user will only care if the issues are mixed (open and closed isses)
+            PrintIssueList(issues, stateWasFiltered);
+        }
+
+        /// <summary>
+        /// Prints the list of issues in a human readable fashion.
+        /// </summary>
+        /// <param name="issues"></param>
+        /// <param name="displayState">If true the state (open/closed) of each issue will be displayed.</param>
+        private static void PrintIssueList(List<Issue> issues, bool displayState)
+        {
+            var formatted = new List<string[]>();
+            foreach (var i in issues)
+            {
+                var tagList = i.Tags.Length > 0 ? "[" + string.Join(", ", i.Tags.Select(t => t.Name)) + "]" : "";
+
+                var cc = i.Comments.Count(c => c.Editable);
+                var comments = cc > 0 ? $" ({cc} comments)" : "";
+                formatted.Add(new[]
+                {
+                    $"#{i.Id}",
+                    displayState ? $"[{i.State}]" : "",
+                    $"'{i.Title}'",
+                    "created by " + i.Author,
+                    $"@ {i.CreationDate.ToShortDateString()}",
+                    comments,
+                    tagList
+                });
+            }
+            Console.WriteLine(ConsoleFormatter.PadElementsInLines(formatted));
+        }
+
+        /// <summary>
+        /// Applies the specific filter on the collection, removing any items that don't match
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="issues"></param>
+        private static void ApplyFilter(FilterValue filter, List<Issue> issues)
+        {
+            switch (filter.FilterType)
+            {
+                case Filter.Tag:
+                    var tags = (Tag[])filter.Value;
+                    issues.RemoveAll(i => i.Tags.ContainsAll(tags));
+                    break;
+                case Filter.IssueState:
+                    var s = (IssueState)filter.Value;
+                    issues.RemoveAll(i =>
+                    {
+                        // remove all open issues; either open or reopened
+                        if ((s == IssueState.Open || s == IssueState.Reopened) &&
+                            (i.State == IssueState.Open || i.State == IssueState.Reopened))
+                        {
+                            return true;
+                        }
+
+                        // remove closed
+                        return s == IssueState.Closed && i.State == IssueState.Closed;
+                    });
+                    break;
+                case Filter.User:
+                    var user = filter.Value.ToString();
+                    issues.RemoveAll(i => i.Author.Equals(user, StringComparison.InvariantCultureIgnoreCase));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void AssertIssueTracker()
+        {
+            if (!WorkingDirectoryIsIssueTracker)
+                throw new NotSupportedException("Command must execute in a issue tracker directory");
         }
 
         /// <summary>
