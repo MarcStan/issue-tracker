699db73f10d9e5c139fed44a128bb0a7314dbf71
MarcStan <marcstan@live.com>
2017-06-12 13:37:11 +0200

    fixed build not copying when target file existed

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 msbuild IssueTracker.sln /t:Build /p:Configuration=Release /nr:false
 
-echo f|xcopy IssueTracker\bin\Release\IssueTracker.exe !Release\issue.exe
+echo f|xcopy IssueTracker\bin\Release\IssueTracker.exe !Release\issue.exe /y
